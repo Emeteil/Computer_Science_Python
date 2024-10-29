efa6816abc9c5f30fdc24e0d1227e5847e1cec92
Emeteil <94739287+Emeteil@users.noreply.github.com>
2024-10-29 13:33:27 +0700

    new commit

--- Лабораторная работа 8/task_1.py
+++ Лабораторная работа 8/task_1.py
@@ -3,8 +3,8 @@ def main():
         1, "a", 3, 4, "b", 6
     ]
     
-    numbers = [i for i in L if type(i) is int]
-    letters = [i for i in L if type(i) is str]
+    numbers = [i for i in L if type(i) is int] # или i.isdigit() если входные данные в виде строки
+    letters = [i for i in L if type(i) is str] # или i.isalpha() если входные данные в виде строки
     
     del L
     
