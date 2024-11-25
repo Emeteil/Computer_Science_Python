9def1cc758e605721d7334d86707f7b6b5357197
Emeteil <94739287+Emeteil@users.noreply.github.com>
2024-11-25 15:20:10 +0700

    lab_12 at work

--- Лабораторная работа 12/task_4.py
+++ Лабораторная работа 12/task_4.py
@@ -1,51 +1,46 @@
 import PySimpleGUI as sg
-import threading
-import random
 
-from requests.exceptions import *
-import requests
-
-def random_org(a: int, b: int) -> int:
-    try:
-        response = requests.get(
-            "https://www.random.org/integers",
-            params = {
-                "num": 1,
-                "min": a,
-                "max": b,
-                "col": 1,
-                "base": 10,
-                "format": "plain",
-            }
-        )
-
-        response.raise_for_status()
-
-        return int(response.text)
-    except (ConnectionError, HTTPError):
-        return 0
-
-def generate_http(window, values) -> None:
-    window["OUTPUT"].update(f"Считаем...")
-    window["MAIN_BUTTON"].update(disabled = True)
-    min = int(values["MIN_GENERATE"])
-    max = int(values["MAX_GENERATE"])
+def number_to_direct(num: int) -> str:
+    if not (-128 <= num <= 127):
+        raise ValueError("number not in (-128, 127)")
     
-    def random_org_thread():
-        global result
-        result = random_org(min, max)
-        window["OUTPUT"].update(f"{result}")
-        window["MAIN_BUTTON"].update(disabled = False)
-    
-    thread = threading.Thread(target = random_org_thread)
-    thread.start()
+    return bin(num & 255)[2:].zfill(8)
 
-def generate_lib(window, values) -> None:
-    min = int(values["MIN_GENERATE"])
-    max = int(values["MAX_GENERATE"])
+def number_to_return(num: int) -> str:
+    if not (-128 <= num <= 127):
+        raise ValueError("number not in (-128, 127)")
     
-    result = random.randint(min, max)
-    window["OUTPUT"].update(f"{result}")
+    return bin(~num & 255)[2:].zfill(8)
+
+def number_to_additional(num: int) -> str:
+    if not (-128 <= num <= 127):
+        raise ValueError("number not in (-128, 127)")
+    
+    return bin((~num + 1) & 255)[2:].zfill(8)
+
+def binary_to_int(num: str) -> int:
+    if any(i not in "01" for i in num):
+        raise ValueError("number non-binary")
+    
+    if num[0] == '0': return int(num, 2)
+    return -((~int(num, 2) & 255) + 1)
+    
+def convert_numbers(window, values) -> None:
+    number = int(values["NUMBER"])
+    
+    direct_code: str = number_to_direct(number)
+    return_code: str = number_to_return(number)
+    additional_code: str = number_to_additional(number)
+    
+    window["DIRECT_CODE"].update(direct_code)
+    window["DIRECT_CODE_INT"].update(binary_to_int(direct_code))
+    
+    window["RETURN_CODE"].update(return_code)
+    window["RETURN_CODE_INT"].update(binary_to_int(return_code))
+    
+    window["ADDITIONAL_CODE"].update(additional_code)
+    window["ADDITIONAL_CODE_INT"].update(binary_to_int(additional_code))
+    window["OUTPUT"].update("")
 
 def main():
     sg.theme("Dark Grey 15")
@@ -53,38 +48,44 @@ def main():
         sg.Column(
             [
                 [sg.Image(source = "task_4.jpg", size = (275, 275))],
+                [sg.Text("Число")],
                 [
-                    sg.Text("Границы рандома"),
-                    sg.InputText(default_text = "0", size = (5, 1), key = "MIN_GENERATE"),
-                    sg.InputText(default_text = "100", size = (5, 1), key = "MAX_GENERATE")
+                    sg.InputText(default_text = "", size = (15, 1), key = "NUMBER")
                 ],
+                [sg.Text("Прямой код")],
                 [
-                    sg.Radio("Псевдорандом", "RADIO1", default = True, key = "RANDOM_LIB"),
-                    sg.Radio("random.org", "RADIO1", key="RANDOM_ORG")
+                    sg.InputText(default_text = "", readonly = True, size = (10, 1), key = "DIRECT_CODE"),
+                    sg.InputText(default_text = "", readonly = True, size = (4, 1), key = "DIRECT_CODE_INT")
                 ],
-                [sg.Button("Сгенерировать", size = (15, 2), key = "MAIN_BUTTON")],
-                [sg.Text("Нажми на кнопку", key = "OUTPUT", justification = 'center')]
+                [sg.Text("Обратный код")],
+                [
+                    sg.InputText(default_text = "", readonly = True, size = (10, 1), key = "RETURN_CODE"),
+                    sg.InputText(default_text = "", readonly = True, size = (4, 1), key = "RETURN_CODE_INT")
+                ],
+                [sg.Text("Дополнительный код")],
+                [
+                    sg.InputText(default_text = "", readonly = True, size = (10, 1), key = "ADDITIONAL_CODE"),
+                    sg.InputText(default_text = "", readonly = True, size = (4, 1), key = "ADDITIONAL_CODE_INT")
+                ],
+                [sg.Button("Перевести", size = (15, 2), key = "MAIN_BUTTON")],
+                [sg.Text("", key = "OUTPUT", justification = 'center')]
             ],
             element_justification = 'center',
             justification = 'center',
         )
     ]]
     
-    window = sg.Window("Рандомайзер", layout, size = (300, 430), icon = "task_4.ico")
+    window = sg.Window("Байт", layout, size = (300, 430), icon = "task_4.ico")
     
     while True:
         event, values = window.read()
         try:
             match event:
                 case "MAIN_BUTTON":                    
-                    if values["RANDOM_LIB"]:
-                        generate_lib(window, values)
-                        continue
-                    
-                    generate_http(window, values)
+                    convert_numbers(window, values)
                 case sg.WIN_CLOSED: break
                 case _: pass
-        except ValueError:
+        except ValueError as e:
             window["OUTPUT"].update("Некоректные данные!")
         except Exception as e:
             window["OUTPUT"].update(str(e))
